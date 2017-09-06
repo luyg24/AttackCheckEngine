6b0e61ccd14294692afc93fe42b888fa5613a062
luyg24@126.com <luyg24@126.com>
2017-09-06 17:22:43 +0800

    modify

--- base64todict.py
+++ base64todict.py
@@ -1,23 +1,44 @@
-#!/usr/bin/env python
-
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
 """
 base64 convert to dict
 """
-
 import base64
+import sys
 
 
-def b64_convertdict(base64string):
-    base64string = base64string
-    string = base64.b64decode(base64string)
-    print string
+def b64convertdict(base64string):
 
-
-b64 = 'R0VUIC9EVldBL3Z1bG5lcmFiaWxpdGllcy9zcWxpLz9pZD0xK3VuaW9uK3NlbGVjdCsxJlN1Ym1pdD1TdWJtaXQgSFRUUC8xLjENCkhvc3Q6IDE\
-5Mi4xNjguMjQuMjUwDQpDb25uZWN0aW9uOiBrZWVwLWFsaXZlDQpVcGdyYWRlLUluc2VjdXJlLVJlcXVlc3RzOiAxDQpVc2VyLUFnZW50OiBNb3ppbGxhL\
-zUuMCAoTWFjaW50b3NoOyBJbnRlbCBNYWMgT1MgWCAxMF8xMF81KSBBcHBsZVdlYktpdC81MzcuMzYgKEtIVE1MLCBsaWtlIEdlY2tvKSBDaHJvbWUvNjAu\
-MC4zMTEyLjExMyBTYWZhcmkvNTM3LjM2DQpBY2NlcHQ6IHRleHQvaHRtbCxhcHBsaWNhdGlvbi94aHRtbCt4bWwsYXBwbGljYXRpb24veG1sO3E9MC45LGl\
-tYWdlL3dlYnAsaW1hZ2UvYXBuZywqLyo7cT0wLjgNClJlZmVyZXI6IGh0dHA6Ly8xOTIuMTY4LjI0LjI1MC9EVldBL3Z1bG5lcmFiaWxpdGllcy9zcWxpLw\
-0KQWNjZXB0LUVuY29kaW5nOiBnemlwLCBkZWZsYXRlDQpBY2NlcHQtTGFuZ3VhZ2U6IHpoLUNOLHpoO3E9MC44LGVuO3E9MC42DQpDb29raWU6IHNlY3Vya\
-XR5PWxvdzsgUEhQU0VTU0lEPWcxcDhqdTZsc2RzOWVhMWhwY3F2NmJ1am81DQoNCg=='
-b64_convertdict(b64)
+    try:
+        request_header = {}
+        post_data = ''
+        base64string = base64string
+        string = base64.b64decode(base64string)
+        b64list = string.split('\r\n')
+        method = b64list[0].split(' ')[0]
+        request_header['method'] = method
+        url = b64list[0].split(' ')[1]
+        request_header['url'] = url
+        protocol = b64list[0].split(' ')[2]
+        request_header['protocol'] = protocol
+        b64list.remove(b64list[0])
+        method = method.upper()
+        if method == 'POST':
+            post_data = b64list[len(b64list)-1]
+            b64list.remove(post_data)
+        for i in range(len(b64list)):
+            if len(b64list[i])>= 3:
+                tmp = b64list[i].split(':')
+                key = tmp[0]
+                value = tmp[1]
+                request_header[key] = value
+        if method == 'POST' or method == 'GET':
+            return request_header
+        else:
+            return 'other method or error!'
+    except Exception, e:
+        print 'base64string: %s, string: %s' % (base64string, string)
+# 从命令行获取base64字符串
+b64 = sys.argv[1]
+# print b64
+print b64convertdict(b64)
