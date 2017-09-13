74e2d5a81276a0f140025f9b819859d82a655148
luyg24@126.com <luyg24@126.com>
2017-09-13 10:12:44 +0800

    modify

--- op_excel.py
+++ op_excel.py
@@ -6,15 +6,18 @@ process excel
 
 import xlwt
 import xlrd
+import commands
+import datetime
+from xlutils.copy import copy
 
-
-wbk = xlrd.open_workbook('test.xls')
-worksheet = wbk.sheet_by_name('test')
-num_rows = worksheet.nrows
-print num_rows
-
-file = open('guanxing_result.txt', 'r')
-wbk = xlwt.Workbook()
-sheet = wbk.add_sheet('test')
-sheet.write(0, 1, 'test')
-wbk.save('test.xlxs')
+def create_excel(filename):
+    file = open(filename, 'r')
+    content = file.readlines()
+    print content
+    # date_filename = str(datetime.date.today())
+    # oldwb = xlrd.open_workbook('tmp_gx.xls', formatting_info=True);
+    # newwb = copy(oldwb);
+    # news = newwb.get_sheet(0);
+    # news.write(3,3,"33test")
+    # news.write(4,3,"43test")
+    # newwb.save(date_filename);
