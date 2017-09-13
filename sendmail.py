e25d5158e93e990e5078c954d21ac6ef059b5131
luyg24@126.com <luyg24@126.com>
2017-09-13 20:01:10 +0800

    modify

--- sendmail.py
+++ sendmail.py
@@ -98,8 +98,6 @@ if __name__=="__main__":
     my.to_list = to_list
     my.cc_list = cc_list
     my.tag = "个人测试"
-    print type(my.cc_list)
-    print my.user, type(my.user)
     my.msg = MIMEText(u'hello你好,send by Python...', 'plain', 'utf-8')
     my.doc = ['2017-09-13']
     # my.doc = ['abc.doc','bcd.doc']
