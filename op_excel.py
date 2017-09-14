98cf2591aaf09d06f5992225624311632bcac795
luyg24@126.com <luyg24@126.com>
2017-09-14 15:27:27 +0800

    modify

--- op_excel.py
+++ op_excel.py
@@ -57,7 +57,11 @@ line =  int(line[0])
 if line > 0:
     content, filename = create_excel('guanxing_result.txt')
     mail_subject = u'观星台审计'
-    result = sendmail_v2.get_conf(mail_subject, content, filename)
-    print result
+    for i in range(10):
+        result = sendmail_v2.get_conf(mail_subject, content, filename)
+        if result == 0:
+            break
+        else:
+            continue
 else:
     print 'nothing!'
