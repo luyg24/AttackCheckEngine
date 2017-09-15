80cd3575c6d27ff05b6654f637540553b347b705
luyg24@126.com <luyg24@126.com>
2017-09-15 17:12:12 +0800

    modify errlog

--- test.py
+++ test.py
@@ -8,9 +8,13 @@ import record_err
 import datetime
 import traceback
 
+def test2():
+    print 'I am test2'
+
 def test():
     try:
         print 'hello'
         print ae
     except Exception as e:
         record_err.logrecord()
+test()
