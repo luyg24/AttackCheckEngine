85816be6e4feb610116101210000d393fb1ec9c3
luyg24@126.com <luyg24@126.com>
2017-09-15 15:08:49 +0800

    add logfile

---
+++ record_err.py
@@ -0,0 +1,28 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+记录异常信息到logs目录
+"""
+import commands
+import datetime
+import re
+
+def logging():
+    result = ''
+    date = datetime.datetime.now().strftime('%Y-%m-%d')
+    #check if exists logs folder
+    status, output = commands.getstatusoutput('ls logs')
+    result = re.search('No such file', output)
+    if result is not None:
+        status, output = commands.getstatusoutput('mkdir logs')
+    # check if exists log file
+    status, output = commands.getstatusoutput('ls ../logs%s' %date)
+    result = re.search('No such file', output)
+    if result is not None:
+        status, output = commands.getstatusoutput('touch logs/%s' %date)
+    logfile = 'logs/' + date
+    return(logfile)
+    # create log file , named by date
+
+    status, output = commands.getstatusoutput('')
+print logging()
