68f2d9177505e0f758de1e87f0eb0b958853c2a0
luyg24@126.com <luyg24@126.com>
2017-09-19 16:06:16 +0800

    modify

--- attack_deliver.py
+++ attack_deliver.py
@@ -13,16 +13,25 @@ filereadlist = []
 xsslist = []
 def catagory(attack_data):
     try:
+        file = open('/tmp/attackinfo.txt', 'a')
+        count = 0
         if attack_data[u'attack_type'] == u'文件读取' and attack_data[u'status'] == 200:
             global filereadlist
             if attack_data not in filereadlist:
                 filereadlist.append(attack_data)
-            print filereadlist
+                count += 1
             # print attack_data
         elif attack_data[u'attack_type'] == u'XSS攻击' and attack_data[u'status'] == 200:
             global xsslist
             if attack_data not in xsslist:
                 xsslist.append(attack_data)
-            print xsslist
+                count += 1
+        else:
+            pass
+        if count > 0:
+            file.write(str(attack_data))
+            file.write('\n')
+            file.close()
+
     except Exception as e:
         record_err.logrecord()
