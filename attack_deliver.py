bfff281ce12e8942c75ac6a3c5b64c1a680dae16
luyg24@126.com <luyg24@126.com>
2017-09-19 15:55:52 +0800

    modify

--- attack_deliver.py
+++ attack_deliver.py
@@ -15,12 +15,14 @@ def catagory(attack_data):
     try:
         if attack_data[u'attack_type'] == u'文件读取' and attack_data[u'status'] == 200:
             global filereadlist
-            filereadlist.append(attack_data)
+            if attack_data not in filereadlist:
+                filereadlist.append(attack_data)
             print filereadlist
             # print attack_data
         elif attack_data[u'attack_type'] == u'XSS攻击' and attack_data[u'status'] == 200:
             global xsslist
-            xsslist.append(attack_data)
+            if attack_data not in xsslist:
+                xsslist.append(attack_data)
             print xsslist
     except Exception as e:
         record_err.logrecord()
