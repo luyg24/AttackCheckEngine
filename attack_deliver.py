a8d3905dd80d00a357521971462f20419dce0ff1
luyg24@126.com <luyg24@126.com>
2017-09-19 15:51:25 +0800

    modify

--- attack_deliver.py
+++ attack_deliver.py
@@ -1,7 +1,7 @@
 # !/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-attack catagory
+attack catagory and delete the same line
 """
 
 import record_err
@@ -9,7 +9,18 @@ import linecache
 import commands
 import time
 
-
+filereadlist = []
+xsslist = []
 def catagory(attack_data):
-    if attack_data[u'attack_type'] == u'文件读取' and attack_data[u'status'] == 200:
-        print attack_data
+    try:
+        if attack_data[u'attack_type'] == u'文件读取' and attack_data[u'status'] == 200:
+            global filereadlist
+            filereadlist.append(attack_data)
+            print filereadlist
+            # print attack_data
+        elif attack_data[u'attack_type'] == u'XSS攻击' and attack_data[u'status'] == 200:
+            global xsslist
+            xsslist.append(attack_data)
+            print xsslist
+    except Exception as e:
+        record_err.logrecord()
