f5b56015d794f5fb9076666480cecd4770fa787f
luyg24@126.com <luyg24@126.com>
2017-09-20 17:33:50 +0800

    modify

--- write_todb.py
+++ write_todb.py
@@ -10,12 +10,19 @@ import linecache
 import commands
 import time
 
+def writedb(conf)
+
+
 def getinfo(filename):
     file = open(filename, 'r')
+    config = {}
     content = file.readlines()
-    print content
+    for i in range(len(content)):
+        tmp = content[i].split(':')
+        print tmp
+    # print content
 
 
 if __name__ == '__main__':
-    print __name__
+    # print __name__
     getinfo('ids_mysql.conf')
