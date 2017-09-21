4fd46e4333067b1cf7b222d338201fab13fb73d8
luyg24@126.com <luyg24@126.com>
2017-09-21 13:57:16 +0800

    modify

--- read_file_mysql_delsame.py
+++ read_file_mysql_delsame.py
@@ -11,12 +11,13 @@ import linecache
 import commands
 import time
 
-filereadlist = []
-xsslist = []
-sqlinject = []
+# filereadlist = []
+# xsslist = []
+# sqlinject = []
 def catagory(attack_data):
     try:
         # load list
+        print load_write_listfile()
         filereadlist, xsslist, sqlinject = load_write_listfile.checkfile()
         print filereadlist, xsslist, sqlinject
         print 'the return'
@@ -24,18 +25,18 @@ def catagory(attack_data):
         # tmp means hostname and url
         tmp = attack_data[u'url'] + attack_data[u'hostname']
         if attack_data[u'attack_type'] == u'文件读取' and attack_data[u'status'] == 200:
-            global filereadlist
+            # global filereadlist
             if tmp not in filereadlist:
                 filereadlist.append(tmp)
                 count += 1
             # print attack_data
         elif attack_data[u'attack_type'] == u'XSS攻击' and attack_data[u'status'] == 200:
-            global xsslist
+            # global xsslist
             if tmp not in xsslist:
                 xsslist.append(tmp)
                 count += 1
         elif attack_data[u'attack_type'] == u'SQL注入' :
-            global sqlinject
+            # global sqlinject
             if tmp not in sqlinject:
                 sqlinject.append(tmp)
                 count += 1
