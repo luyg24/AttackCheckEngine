8f826eff997768bf13ecec0dafbc60956aee4079
luyg24@126.com <luyg24@126.com>
2017-09-21 19:20:32 +0800

    modify

--- read_mysql.py
+++ read_mysql.py
@@ -24,6 +24,7 @@ def countsql():
 
 def readfile(startid = 1):
     try:
+        print startid
         config = getinfo(filename)
         conn = mysql.connector.connect(**config)
         cur = conn.cursor()
