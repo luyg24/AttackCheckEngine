c4b795a7855948d02cb8b01ee3609fc2ca9fd33c
luyg24@126.com <luyg24@126.com>
2017-09-21 20:12:26 +0800

    modify

--- check_fileread_attack.py
+++ check_fileread_attack.py
@@ -4,5 +4,12 @@
 check file reading type attack! return id and vul
 """
 
+import base64
+
+
 def check(id, hostname, url, method, status, postdata):
-    print hostname, url, status
+    url = base64.b64decode(url)
+    print hostname, url, status, method
+    if method.lower() == 'post':
+        postdata = base64.b64decode(postdata)
+        print postdata
