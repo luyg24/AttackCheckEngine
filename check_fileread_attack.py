06ef86958a7d19cff5fb2795fc492f908d2b97d7
luyg24@126.com <luyg24@126.com>
2017-09-22 14:30:00 +0800

    modify

--- check_fileread_attack.py
+++ check_fileread_attack.py
@@ -10,22 +10,27 @@ import record_err
 
 def check(id, hostname, url, method, status, postdata):
     try:
-        print id, hostname, url, method, status, postdata
-        print type(id), type(hostname), type(url), type(method), type(status), type(postdata)
+        # print id, hostname, url, method, status, postdata
+        # print type(id), type(hostname), type(url), type(method), type(status), type(postdata)
         url = base64.b64decode(url)
         headers = {'user-agent':'Chrome/60.0.3112.113 Safarids24/537.36'}
-        print method.lower()
         if method.lower() == 'post':
             postdata = base64.b64decode(postdata)
             # print postdata
         if method.lower() == 'get':
             httpurl = 'http://' + hostname + url
             httpsurl = 'https://' + hostname + url
-            print httpurl, httpsurl
             r = requests.get(httpurl, headers)
-            print 'httpurl', r.text
-            r = requests.get(httpsurl, headers)
-            print 'httpsurl', r.text
+            httpcontent = r.text
+            httpscontent = requests.get(httpsurl, headers)
+            httpscontent = r.text
+            content_process(httpcontent, httpscontent)
+    except Exception as e:
+        record_err.logrecord()
+
+def content_process(httpcontent = '', httpscontent = ''):
+    try:
+        print httpcontent, httpscontent
     except Exception as e:
         record_err.logrecord()
 
