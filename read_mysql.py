553ab17e3e38a352fa0da44580420161f74eaadf
luyg24@126.com <luyg24@126.com>
2017-09-22 11:37:47 +0800

    modify

--- read_mysql.py
+++ read_mysql.py
@@ -22,6 +22,7 @@ def catfileread(id, hostname, url, method, status, postdata):
     postdata = str(postdata)
     if status == 200:
         print 'need check!'
+        print url
         check_fileread_attack.check(id, hostname, url, method, status, postdata)
         # status is 200 check or no check!
     elif status != 200:
