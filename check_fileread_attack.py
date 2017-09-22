df4d4597a79eb81423bbbfbcf3364105009d7fb4
luyg24@126.com <luyg24@126.com>
2017-09-22 11:32:35 +0800

    modify

--- check_fileread_attack.py
+++ check_fileread_attack.py
@@ -6,21 +6,26 @@ check file reading type attack! return id and vul
 
 import base64
 import requests
+import record_err
 
 def check(id, hostname, url, method, status, postdata):
-    print id, hostname, url, method, status, postdata
-    print type(id), type(hostname), type(url), type(method), type(status), type(postdata)
-    url = base64.b64decode(url)
-    headers = {'user-agent':'Chrome/60.0.3112.113 Safarids24/537.36'}
-    # print hostname, url, status, method
-    if method.lower() == 'post':
-        postdata = base64.b64decode(postdata)
-        # print postdata
-    if method.lower() == 'get':
-        httpurl = 'http://' + hostname + url
-        httpsurl = 'https://' + hostname + url
-        r = requests.get(httpurl, headers)
-        print 'httpurl', r.text
-        r = requests.get(httpsurl, headers)
-        print 'httpsurl', r.text
+    try:
+        print id, hostname, url, method, status, postdata
+        print type(id), type(hostname), type(url), type(method), type(status), type(postdata)
+        url = base64.b64decode(url)
+        headers = {'user-agent':'Chrome/60.0.3112.113 Safarids24/537.36'}
+        # print hostname, url, status, method
+        if method.lower() == 'post':
+            postdata = base64.b64decode(postdata)
+            # print postdata
+        if method.lower() == 'get':
+            httpurl = 'http://' + hostname + url
+            httpsurl = 'https://' + hostname + url
+            print httpurl, httpsurl
+            r = requests.get(httpurl, headers)
+            print 'httpurl', r.text
+            r = requests.get(httpsurl, headers)
+            print 'httpsurl', r.text
+    except Exception as e:
+        record_err.logrecord()
 
