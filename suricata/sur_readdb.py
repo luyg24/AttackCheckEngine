cb39052fa5d0c69b6477a5cb979d09f01b5d1752
luyg24@126.com <luyg24@126.com>
2017-09-29 16:37:46 +0800

    read db

---
+++ suricata/sur_readdb.py
@@ -0,0 +1,118 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+read from mysql
+"""
+
+import update_msyql_vul
+import check_fileread_attack
+import mysql.connector
+import record_err
+import base64
+
+
+
+def catfileread(id, hostname, url, method, status, postdata):
+    # unicode change to str
+    hostname = str(hostname)
+    url = str(url)
+    method = str(method)
+    postdata = str(postdata)
+    if status == 200:
+        check_fileread_attack.check(id, hostname, url, method, status, postdata)
+        # status is 200 check or no check!
+    elif status != 200:
+        print status
+        vul = 'N'
+        update_msyql_vul.updatevul(id, vul)
+
+    else:
+        print 'what?'
+
+
+def readfile(startid = 1):
+    try:
+        id = startid
+        config = getinfo(myconf)
+        conn = mysql.connector.connect(**config)
+        cur = conn.cursor()
+        readsql = 'select  catagory, hostname, url, method, status, postdata, payload from httpattack where id = %d' % startid
+        cur.execute(readsql)
+        # get info
+        result = cur.fetchall()
+        print result
+        if len(result) > 0:
+            attack_type = result[0][0]
+            hostname = result[0][1]
+            url = result[0][2]
+            method = result[0][3]
+            status = result[0][4]
+            postdata = result[0][5]
+        else:
+            print 'mysql info error'
+        if attack_type == u'文件读取':
+            pass
+            # catfileread(id, hostname, url, method, status, postdata)
+        else:
+            # process other attack_type
+            pass
+
+    except Exception as e:
+        record_err.logrecord()
+
+def getinfo(filename):
+    try:
+        file = open(filename, 'r')
+        config = {}
+        content = file.readlines()
+        for i in range(len(content)):
+            tmp = content[i].split(':')
+            key = tmp[0]
+            value = tmp[1].split('\n')[0]
+            config[key] = value
+        return config
+    except Exception as e:
+        record_err.logrecord()
+
+def getcount():
+    try:
+        # get msyql line number
+        config = getinfo(myconf)
+        conn = mysql.connector.connect(**config)
+        cur = conn.cursor()
+        linecount = 'select count(id) from ids_info '
+        cur.execute(linecount)
+        result = cur.fetchall()
+        return int(result[0][0])
+        cur.close()
+        conn.close()
+    except Exception as e:
+        record_err.logrecord()
+
+
+def getuntestline():
+    try:
+        # get the untest id
+        count = getcount()
+        for i in range(count):
+            config = getinfo(myconf)
+            conn = mysql.connector.connect(**config)
+            cur = conn.cursor()
+            readsql = 'select id from ids_info where attack_status is NULL limit %d,1' % i
+            cur.execute(readsql)
+            result = cur.fetchall()
+            id = result[0][0]
+            # return id
+            cur.close()
+            conn.close()
+            readfile(id)
+    except Exception as e:
+        record_err.logrecord()
+
+
+if __name__ == '__main__':
+    myconf = 'ids_mysql.conf'
+    # print __name__
+    getuntestline()
+    # print startid
+
